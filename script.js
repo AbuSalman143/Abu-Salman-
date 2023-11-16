6b4290718395ad41c782a404a0d705ae3d95ca2f
Abu Salman <121804012+AbuSalman143@users.noreply.github.com>
2023-11-17 00:23:35 +0530

    My portfolio

---
+++ script.js
@@ -0,0 +1,31 @@
+let menuIcon=document.querySelector("#menu-icon");
+let navbar=document.querySelector(".navbar");
+
+menuIcon.addEventListener("click",toggel);
+function toggel(){
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+}
+
+
+let home=document.querySelector("#nov-home");
+let about=document.querySelector("#nov-about");
+let services=document.querySelector("#nov-services");
+let project=document.querySelector("#nov-project");
+let contact=document.querySelector("#nov-contact");
+
+
+home.addEventListener("click",removeToggel);
+about.addEventListener("click",removeToggel);
+services.addEventListener("click",removeToggel);
+project.addEventListener("click",removeToggel);
+contact.addEventListener("click",removeToggel);
+
+function removeToggel(){
+    menuIcon.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+}
+
+
+
+
